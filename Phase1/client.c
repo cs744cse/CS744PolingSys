d4c941ff8da15a3dfbca793e8081f9896c13389f
cs744iitb <31162684+cs744iitb@users.noreply.github.com>
2017-09-01 19:16:44 +0530

    Add files via upload

---
+++ Phase1/client.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+#include <string.h>
+#include <stdlib.h>
+
+void tokenize(char *);
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+int count ;
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    
+    printf("Please enter the message: ");
+    
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    
+    n = write(sockfd,buffer,strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    
+    bzero(buffer,256);
+    
+    n = read(sockfd,buffer,255);
+    char *verif = (char *)malloc((n+1)*sizeof(*verif));
+    strncpy(verif, buffer,n);
+    verif[n] = '\0';
+    if(!(strcmp(verif,"FAILED")))
+    {
+        printf("Authentication Failed.\nPlease Retry.\n");
+        exit(1);
+    }
+    printf("Region : ");
+    
+    if (n < 0) 
+         error("ERROR reading from socket");
+    
+    printf("%s\n",buffer);
+
+    n = read(sockfd,buffer,256);
+    
+    printf("Candidate List :\n");
+    
+    if (n < 0) 
+         error("ERROR reading from socket");
+
+    tokenize(buffer);
+    int choice;
+    char ch[2];
+    printf("\nChoose Your Candidate : ");
+     scanf("%d",&choice);
+     if(choice > count)
+     {
+         error("Invalid Selection");
+         exit(1);
+     }
+     sprintf(ch,"%d",choice);
+    //printf("Candidate ",);
+    //printf("%s %d ",verif,strlen(verif));
+    char *selected = NULL;
+    // char c=(char)choice;
+     
+    selected = (char*)malloc(strlen(ch)+strlen(verif)*sizeof(char));
+    strncpy(selected,ch,2);
+    strcat(selected," ");
+    strcat(selected,verif);
+    printf(" %s ",selected);
+    n = write(sockfd,selected,strlen(selected));
+    if(n<0)
+    {
+        error("Writing Error:");
+        exit(1);
+    }
+    bzero(buffer,256);
+    n = read(sockfd,buffer,256);
+    printf("\n%s\n",buffer);
+    return 0;
+}
+
+
+
+ void tokenize(char *list)
+ {
+    char delim[] = "$";
+    char* token;
+    
+        for (token = strtok(list, delim); token; token = strtok(NULL, delim))
+        {
+            printf("%c : %s\n", token[0], (token+1));
+            count++;
+        }
+ }
---
+++ Phase1/testdb.sql
@@ -0,0 +1,77 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for Linux (x86_64)
+--
+-- Host: localhost    Database: testdb
+-- ------------------------------------------------------
+-- Server version	5.7.19-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `candidates`
+--
+
+DROP TABLE IF EXISTS `candidates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `candidates` (
+  `id` int(11) NOT NULL,
+  `region` varchar(20) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `votes` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`,`region`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `candidates`
+--
+
+LOCK TABLES `candidates` WRITE;
+/*!40000 ALTER TABLE `candidates` DISABLE KEYS */;
+INSERT INTO `candidates` VALUES (1,'BHANDUP','HIMANSHU GUPTA',0),(1,'MUMBAI','RAM',0),(1,'POWAI','SURAJ',0),(1,'TROMBAY','SURESH',0),(2,'BHANDUP','HIMANSHU UPRETI',3),(2,'MUMBAI','SHYAM',0),(2,'POWAI','SUMIT',0),(2,'TROMBAY','MAHESH',0),(3,'BHANDUP','HIMANSHU UPRETI',1),(3,'MUMBAI','NARESH',0),(3,'POWAI','SAURABH GUPTA',0),(3,'TROMBAY','ROHIT',0),(4,'MUMBAI','SURESH',0),(4,'POWAI','GAURAV GUPTA',0),(4,'TROMBAY','PRASAD',0),(5,'TROMBAY','DIGVIJAY',1);
+/*!40000 ALTER TABLE `candidates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login` (
+  `id` varchar(12) NOT NULL,
+  `region` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES ('123456789123','MUMBAI'),('123459865452','Trombay'),('125478896523','Powai'),('145258789563','Bombay'),('258369147258','Mumbai'),('741852963741','POWAI'),('963852741963','BHANDUP'),('987654321987','TROMBAY');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-09-01 19:16:04
